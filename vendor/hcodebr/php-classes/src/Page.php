e2fd3180f269d26d9f6aeef2e27ca53b2587bc09
eversonlast <eversonlast@yahoo.com.br>
2019-04-03 16:46:30 -0300

    admin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Hcode;
 
 use Rain\Tpl;
@@ -12,12 +11,12 @@ class Page
 		"data"=>[]
 	];
 
-	public function __construct($opts = array()){
+	public function __construct($opts = array(), $tpl_dir="/views/"){
 
 	$this->options= array_merge($this->defaults, $opts);
 
 	$config = array(
-				"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+				"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
 				"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/"
 				 );
 
@@ -52,3 +51,5 @@ class Page
 
 	}
 }
+
+?>
