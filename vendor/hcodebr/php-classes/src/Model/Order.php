79963a5a7204a1b6e62f7a052e26d88eceafef41
eversonlast <eversonlast@yahoo.com.br>
2019-05-08 09:53:16 -0300

    Admin - Pedidos - Busca e Paginação

--- vendor/hcodebr/php-classes/src/Model/Order.php
+++ vendor/hcodebr/php-classes/src/Model/Order.php
@@ -124,6 +124,62 @@ class Order extends Model{
 		$_SESSION[Order::SUCCESS]=NULL;
 	}
 
+	 public static function getPage($page=1, $itemsPerPage = 3){
+
+	 	$start = ($page-1)*$itemsPerPage;	
+
+	 	$sql= new SQL();
+
+	 	$results = $sql->select("
+	 		SELECT sql_calc_found_rows *
+			FROM tb_orders a 
+			INNER JOIN tb_ordersstatus b USING(idstatus)
+			INNER JOIN tb_carts c USING(idcart)
+			INNER JOIN tb_users d ON d.iduser = a.iduser
+			INNER JOIN tb_addresses e USING(idaddress)
+			INNER JOIN tb_persons f ON f.idperson = d.idperson
+			ORDER BY a.dtregister DESC
+			LIMIT $start, $itemsPerPage; 
+			");
+
+			$resultsTotal = $sql->select("SELECT found_rows() AS nrtotal;");
+
+			return [
+				'data'=>$results,
+				'total'=>$resultsTotal[0]["nrtotal"],
+				'pages'=>ceil($resultsTotal[0]["nrtotal"]/$itemsPerPage)
+			];
+	 }
+
+	 public static function getPageSearch($search, $page=1, $itemsPerPage = 3){
+
+	 	$start = ($page-1)*$itemsPerPage;	
+
+	 	$sql= new SQL();
+
+	 	$results = $sql->select("
+	 		SELECT sql_calc_found_rows *
+			FROM tb_orders a 
+			INNER JOIN tb_ordersstatus b USING(idstatus)
+			INNER JOIN tb_carts c USING(idcart)
+			INNER JOIN tb_users d ON d.iduser = a.iduser
+			INNER JOIN tb_addresses e USING(idaddress)
+			INNER JOIN tb_persons f ON f.idperson = d.idperson
+			WHERE a.idorder = :id OR f.desperson LIKE :search
+			ORDER BY a.dtregister DESC
+			LIMIT $start, $itemsPerPage;",[
+				':search'=> '%'.$search.'%',
+				':id'=>$search
+			]);
+
+			$resultsTotal = $sql->select("SELECT found_rows() AS nrtotal;");
+
+			return [
+				'data'=>$results,
+				'total'=>$resultsTotal[0]["nrtotal"],
+				'pages'=>ceil($resultsTotal[0]["nrtotal"]/$itemsPerPage)
+			];
+	 }
 }
 
 ?>
